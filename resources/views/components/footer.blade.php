fc2fe9be817c2cccc78b227e6d790698a375f862
Hafidlul Muffid <hafidlulhidayat17@gmail.com>
2025-10-23 21:28:09 +0700

    perubahan 1

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="text-center border-top fixed-bottom d-flex align-items-center justify-content-center" style="height: 50px; background-color: #F9AFBC;">
-  <p class="mb-0 text-white small">&copy; 2025 Donutlicious | All rights reserved</p>
+  <p class="mb-0 text-white small">&copy; 2025 Donutlicious | Hafidlul_2095</p>
 </footer>
